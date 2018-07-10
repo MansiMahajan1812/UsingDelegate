912bd0c4361a8db3674a9bd1eb1c8e7a8b239033
Mansi Mahajan <Mansi@Amrits-Mac-Mini.local>
2018-07-10 10:38:15 +0530

    Changes committed

--- UsingDelegate/ViewController.swift
+++ UsingDelegate/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, someDelegate {
+   
+@IBOutlet weak var labelForMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    //var vc = self.storyboard?.instantiateViewController(withIdentifier: "nextViewController") as! NextViewController
 
+    }
+    
+    
+    func valueLable(update : String){
+        self.labelForMessage.text = update
+    }
+
+    @IBAction func SubmitAction(_ sender: UIButton) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "nextViewController") as! NextViewController
+        vc.delegate = self
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+   
+    
+    
 }
 
